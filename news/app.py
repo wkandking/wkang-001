c720a5b314db857fb46f3a31202ad8ac1285c247
wkandking <1324812223@qq.com>
2018-03-24 18:15:33 +0800

    Add files via upload

--- news/app.py
+++ news/app.py
@@ -1,10 +1,39 @@
 from flask import Flask,render_template,abort
-import os,json
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
 app.config['TEMPLATES_AUTO_RELODA']=True
-path='/home/shiyanlou/files/'
-dir_list=os.listdir(path)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/shiyanlou'
+db=SQLAlchemy(app)
+
+
+class File(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	title=db.Column(db.String(80))
+	created_time=db.Column(db.DateTime)
+	category_id=db.Column(db.Integer,db.ForeignKey('category.id'))
+	category=db.relationship('Category',backref=db.backref('files'))
+	content=db.Column(db.Text)
+	def __init__(self,title,created_time,category,content):
+		self.title=title
+		self.created_time=created_time
+		self.category=category
+		self.content=content
+	def __repr__(self):
+		return '<File %s>'%self.title
+
+class Category(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String(80))
+	def __init__(self,name):
+		self.name=name
+	def __repr__(self):
+		return '<Category %s>'%self.name
+
+titles=File.query.all()
+category1=titles[0]
+category2=titles[1]
 
 @app.errorhandler(404)
 def not_found(error):
@@ -12,27 +41,31 @@ def not_found(error):
 
 @app.route('/')
 def index():
-	filename_json=[]
-	title_list=[]
-	for filename in dir_list:
-		filename_json.append(path+filename)
-		with open(path+filename) as file:
-			data=json.loads(file.read())
-			title_list.append(data['title'])
-	return render_template('index.html',filename_json=filename_json,title_list=title_list)
+	title={
+	'title1':category1.title,
+	'title2':category2.title
+	}
+	return render_template('index.html',title=title,titles=titles)
 
 
 
 
-@app.route('/files/<filename>')
-def file(filename):
-	filename=filename+'.json'
-	data=[]
-	if filename in dir_list:
-		with open(path+filename) as file:
-			data=json.loads(file.read())
-			print(data)
-		return render_template('file.html',file_data=data)
+@app.route('/files/<file_id>')
+def file(file_id):
+	content={
+	'title1':category1.title,
+	'created_time1':category1.created_time,
+	'category_id1':category1.category_id,
+	'content1':category1.content,
+	'title2':category2.title,
+	'created_time2':category2.created_time,
+	'category_id2':category2.category_id,
+	'content2':category2.content
+	}
+	print(type(file_id))
+	print(type(category1.id))
+	if file_id == str(category1.id) or file_id == str(category2.id):
+		return render_template('file.html',content=content,file_id=file_id)
 	else:
 		abort(404)
 
