ba645bb6e6f933c8073aa208532f85998378fc70
wkandking <1324812223@qq.com>
2018-04-11 19:15:22 +0800

    Add files via upload

--- items.py
+++ items.py
@@ -12,3 +12,6 @@ class WkangItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     update_time=scrapy.Field()
+    commits=scrapy.Field()
+    branches=scrapy.Field()
+    releases=scrapy.Field()
